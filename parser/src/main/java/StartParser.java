4803eddda4417bf655f2101c0fd43b0d6bc51e55
124O <chuffey.1240@yandex.ru>
2016-03-14 23:15:45 +0300

    parser work

--- parser/src/main/java/StartParser.java
+++ parser/src/main/java/StartParser.java
@@ -1,3 +1,4 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,62 +19,69 @@ import java.util.regex.Pattern;
 public class StartParser {
 
     public static void main(String[] args) throws IOException {
-        URL url;
-        InputStream is = null;
-        BufferedReader br;
-        String line;
-        String html = "";
-        String home = "http://seasonvar.ru";
-//        try {
-//            url = new URL(home);
-//            is = url.openStream();  // throws an IOException
-//            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-//
-//            while ((line = br.readLine()) != null) {
-//                html += line;
-//            }
-//        } catch (MalformedURLException mue) {
-//            mue.printStackTrace();
-//        } catch (IOException ioe) {
-//            ioe.printStackTrace();
-//        } finally {
-//            try {
-//                if (is != null) is.close();
-//            } catch (IOException ioe) {
-//                // nothing to see here
-//            }
-//        }
-//        Document parse = Jsoup.parse(html);
-        Document parse = Jsoup.connect(home).get();
-        Elements elements = parse.select("a.betterT");
-        for (Element element : elements) {
-            String urlSerial = home + element.attributes().get("href");
-            Document document = Jsoup.connect(urlSerial).get();
-            String info = document.select("div.pg-s-rb").select("p").get(0).text();
-            String urlImage = document.select("div.full-news-img").select("img").get(0).attributes().get("src");
-            String text = document.select("div.seasonlist").last().select("a").text();
-            text = text.replaceAll("\\>", "");
-            String title = "";
-            String date = "";
-            String episode = "";
-            text = text.split("[СC][еe]ри[аa]л")[text.split("[CС][еe]ри[аa]л").length - 1];
-            if (text.indexOf("с[eе]зон") > -1) {
-                Matcher matcher = Pattern.compile(" [0-9] сезон ").matcher(text);
-                if (matcher.find()) {
-                    String sezon = matcher.group(0).replace(" ", "").replace("сезон", "");
+        String home = "http://myseries.ru/";
+        String seriesEndPoint = "series";
+        String seriesFilter = "page";
+        Document parse = Jsoup.connect(String.format("%s%s?%s=%s", home, seriesEndPoint, seriesFilter, 1)).get();
+        Elements elements = parse.select("div.page-info");
+        String[] split = elements.get(0).text().split("РёР·");
+        Integer pages = Integer.valueOf(split[1].replace(" ", ""));
+        for (int i = 1; i <= pages; i++) {
+            Document doc = Jsoup.connect(String.format("%s%s?%s=%s", home, seriesEndPoint, seriesFilter, i)).get();
+            Elements movies = doc.select("div.movie-about");
+            for (Element movie : movies) {
+                String attr = movie.select("a").attr("href");
+                System.out.println("code " + attr.split("/")[2]);
+                Document serial = Jsoup.connect(String.format("%s%s", home, attr)).get();
+                System.out.println("name " + serial.select("h2.page-heading").text());
+                String url = serial.select("div.movie-image").select("img").attr("src");
+                System.out.println(url);
+                String status = serial.select("p.movie-option").get(0).select("span").text();
+                String chanel = serial.select("p.movie-option").get(1).select("span").text();
+                String type = serial.select("p.movie-option").get(2).select("span").text();
+                String startDate = serial.select("p.movie-option").get(3).select("span").text();
+                System.out.println(status);
+                System.out.println(chanel);
+                System.out.println(type);
+                System.out.println(startDate);
+                String des = serial.select("div.movie-description").select("p").text();
+                System.out.println(des);
+                Elements bottom = serial.select("div.movie-bottom").select("p.movie-option");
+                String imdb = bottom.get(0).select("span.imdb").text();
+                String tvmaze = bottom.get(0).select("span.tvmaze").text();
+                String myshows = bottom.get(0).select("span.myshows").text();
+                System.out.println(imdb);
+                System.out.println(tvmaze);
+                System.out.println(myshows);
+
+                Elements translate = bottom.get(1).select("span");
+                for (Element element : translate) {
+                    System.out.println(element.text());
                 }
-                String[] strings = text.split(" [0-9] сезон");
-                title = strings[0];
-                String[] split = strings[1].replace("(", "").replace(")", "").split(" ");
-                date = split[0];
-                episode = split[1];
-            } else {
-                String[] split = text.split(" \\(");
-                title = split[0];
-                date = split[1].split(" ")[0];
-                episode = split[1].replace(split[1].split(" ")[0], "").replace(" серия)", "").replace(")", "");
+                Elements seasons = serial.select("div.wrapper-white").select("div.panel-default");
+                for (Element season : seasons) {
+                    String seasonNumber = season.select("h3.panel-title").text();
+                    System.out.println(seasonNumber);
+
+                    Elements episodes = season.select("table.table").select("tr");
+                    episodes.remove(0);
+                    for (Element episode : episodes) {
+                        Elements tds = episode.select("td");
+                        String eCode = tds.get(0).text();
+                        String eName = tds.get(1).text();
+                        String eRating = tds.get(tds.size() - 1).text();
+                        String eDate = tds.get(tds.size() - 2).text();
+                        System.out.println(eCode + " " + eName + " " + eRating + " " + eDate);
+                        for (Element td : tds) {
+                            if (td.attr("class").equals("rg-column-td")) {
+                                if (!td.text().isEmpty())
+                                    System.out.println("TranslateTeam " + td.text());
+                            }
+                        }
+                    }
+                }
             }
-            System.out.println(title + " " + date + " " + episode);
+            System.out.println("----------------------------------------------------------");
         }
 
     }
